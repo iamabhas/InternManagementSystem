7673987cc1f1c566a865559c0fe8c81f0291d5ae
iamabhas <abhas123mnm@gmail.com>
2024-04-11 16:33:33 +0545

    refactor: Styled and setup login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Typography } from "@mui/material";
 import LoginComponent from "../components/authComponents/LoginComponent";
 
 const LoginPage = () => {
   return (
     <>
-      <Typography className="center-text" sx={{ m: 2 }} variant="h5">
-        Welcome to Login Page
-      </Typography>
       <div>
         <LoginComponent />
       </div>
