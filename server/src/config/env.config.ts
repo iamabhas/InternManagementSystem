ef78237b1266ce7cb3a00ef77aa31d113aee38fe
iamabhas <abhas123mnm@gmail.com>
2024-04-11 11:55:46 +0545

    refactor: added absolute path for loading env file

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -1,8 +1,19 @@
-import dotenv from "dotenv";
-dotenv.config();
+import * as dotenv from "dotenv";
+import path from "path";
+import Logger from "../lib/logger";
+
+const result = dotenv.config();
+
+if (result.error) {
+  Logger.error(`'.env' file could not be loaded : ${result.error}`);
+  throw result.error;
+}
+
+dotenv.config({ path: path.resolve(__dirname, "../config/.env") });
 
 const envConfig = {
   serverPort: process.env.SERVER_PORT,
+  mongodbConnectionString: process.env.MONGODB_CONNECTION_STRING,
 };
 
 export default envConfig;
