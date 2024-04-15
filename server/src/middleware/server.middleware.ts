c2216c282067f0fad6ed16fa2bcabb0abea4584a
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-04-15 15:05:57 +0545

    feat: add cookieParser and configure CORS

--- server/src/middleware/server.middleware.ts
+++ server/src/middleware/server.middleware.ts
@@ -1,11 +1,19 @@
 import express, { Application } from "express";
 import morganMiddleware from "./morgan.middleware";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 export const initializeMiddlewares = (
   expressApplication: Application
 ): void => {
-  expressApplication.use(cors());
   expressApplication.use(express.json());
   expressApplication.use(morganMiddleware);
+  expressApplication.use(cors(corsOptions));
+  expressApplication.use(cookieParser());
 };
