f009a89478cef85d3b8e9979588e67c0b2819139
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-04-15 15:07:53 +0545

    feat: Service layer for axios method

---
+++ client/src/services/ApiCall.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const commonRequest = async (methods, url, body, header) => {
+  let config = {
+    method: methods,
+    url,
+    headers: header
+      ? header
+      : {
+          "Content-Type": "application/json",
+        },
+    data: body ? body : "",
+  };
+  return axios(config)
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
