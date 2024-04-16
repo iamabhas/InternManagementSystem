c82aa102ad4283aeac91ed2cdf4c2981ae3a8cf1
iamabhas <abhas123mnm@gmail.com>
2024-04-16 11:16:36 +0545

    refactor: used function for missing field validation message, set necessary fields as required

--- server/src/database/schema/user.schema.ts
+++ server/src/database/schema/user.schema.ts
@@ -1,30 +1,40 @@
 import mongoose from "mongoose";
+import { fieldCannotBeEmpty } from "../../../utils/schemaUtils/userschemaMessage";
+import { roleConstants } from "../../constants/roleConstants";
+
+const { ADMIN, SUPER_ADMIN, USER } = roleConstants;
+
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, "this field cannot be empty"],
+      required: [true, fieldCannotBeEmpty("username")],
       unique: true,
     },
     email: {
       type: String,
       unique: true,
       lowercase: true,
+      required: [true, fieldCannotBeEmpty("email")],
     },
     phoneNo: {
       type: Number,
       unique: true,
       min: 9,
       max: 10,
+      required: [true, fieldCannotBeEmpty("phoneNo")],
     },
     role: {
       type: String,
-      enum: ["user", "admin", "superAdmin"],
-      default: "user",
+      enum: [USER, ADMIN, SUPER_ADMIN],
+      default: USER,
+      required: [true, fieldCannotBeEmpty("role")],
     },
     password: {
       type: String,
-      required: [true, "this field cannot be empty"],
+      min: 5,
+      max: 50,
+      required: [true, fieldCannotBeEmpty("password")],
     },
   },
   { timestamps: true }
