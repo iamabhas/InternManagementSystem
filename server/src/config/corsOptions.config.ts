073bb0b03e160385f0d45888e999087a8f4092ab
iamabhas <abhas123mnm@gmail.com>
2024-04-16 11:41:39 +0545

    feat: cors options config for cors middleware configuration

---
+++ server/src/config/corsOptions.config.ts
@@ -0,0 +1,5 @@
+export const corsOptionsConfig = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
