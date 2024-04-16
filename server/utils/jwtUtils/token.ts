647a048e93e2dd1166b932ab2cf5fb45ae0b5c4d
iamabhas <abhas123mnm@gmail.com>
2024-04-16 11:18:25 +0545

    renamed JWT folder to jwtUtils

---
+++ server/utils/jwtUtils/token.ts
@@ -0,0 +1,38 @@
+import { IPayload } from "../../@types/interface/RequestBody";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+dotenv.config();
+
+export const AccessToken = async (
+  user_id: mongoose.Types.ObjectId,
+  username: string,
+  role: string
+): Promise<string> => {
+  const payload: Required<IPayload> = {
+    user_id: user_id,
+    username: username,
+    role: role,
+  };
+
+  const secretKey = process.env.ACCESS_TOKEN_SECRET as string;
+  const expiratonTimeInSeconds = 30 * 60; // 30 minutes in seconds
+
+  const options: jwt.SignOptions = {
+    issuer: "Inter Management System",
+    expiresIn: expiratonTimeInSeconds,
+  };
+
+  try {
+    const token = jwt.sign(payload, secretKey, options);
+    return token;
+  } catch (error) {
+    if (error instanceof JsonWebTokenError) {
+      throw new Error("JSON WEB TOKEN ERROR");
+    } else {
+      throw error;
+    }
+  }
+};
