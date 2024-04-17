d7d849bcc44de67045f3f4e5ddd2eeb360794500
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-04-17 12:25:25 +0545

    fix: fix experties field of mentor

--- server/src/database/schema/mentor.schema.ts
+++ server/src/database/schema/mentor.schema.ts
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 import { fieldCannotBeEmpty } from "../../utils/schemaUtils/userSchemaMessage";
-
+import { positionConstants } from "../../constants/roleConstants";
+const { SOFTWARE_ENGINEER, SCRUM_MASTER } = positionConstants;
 const mentorSchema = new mongoose.Schema({
-    mentorName:{
-         type:String,
-         required:[true, fieldCannotBeEmpty('MentorName')]
-    },email: {
-        type: String,
-        unique: true,
-        lowercase: true,
-        required: [true, fieldCannotBeEmpty("email")],
-      },expertise:{
-        type:String,
-        required: [true, fieldCannotBeEmpty("expertise")],
-      },role:{
-        type:String,
-        enum:['programmer','scrumMaster']
-      }
-})
+  mentorName: {
+    type: String,
+    required: [true, fieldCannotBeEmpty("MentorName")],
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [true, fieldCannotBeEmpty("email")],
+  },
+  expertise: {
+    type: [String],
+    required: [true, fieldCannotBeEmpty("expertise")],
+  },
+  role: {
+    type: String,
+    enum: [SOFTWARE_ENGINEER, SCRUM_MASTER],
+  },
+});
 
-const Mentor = mongoose.model('mentor', mentorSchema)
+const Mentor = mongoose.model("mentor", mentorSchema);
 
-export default Mentor
+export default Mentor;
