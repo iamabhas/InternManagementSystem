17baeb7f30cc9de322dd04fb64d478cec19250db
iamabhas <abhas123mnm@gmail.com>
2024-04-17 15:12:46 +0545

    feat: Dashboard components for users

---
+++ client/src/components/dashboardComponents/AdminComponents/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const Admin = () => {
+  return (
+    <main>
+      <Typography variant="h6">Welcome to Dashboard !</Typography>
+    </main>
+  );
+};
+
+export default Admin;
