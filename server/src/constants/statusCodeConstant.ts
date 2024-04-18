d63e3f0e8b5cf73e285afc50e5895e0bbef1d6b7
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-04-18 16:12:25 +0545

    fix: remove unnecessary file constant and combine it into one

---
+++ server/src/constants/statusCodeConstant.ts
@@ -0,0 +1,15 @@
+export enum FourErrorConstant {
+  VALIDATION_ERROR = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+}
+
+export enum TwoResponseConstant {
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NON_AUTHORITATIVE_INFORMATION = 203,
+  NO_CONTENT = 204,
+}
