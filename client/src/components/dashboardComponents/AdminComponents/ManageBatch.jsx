fad963d5d3bf7acd5101e0bffb06924ec82c64d8
iamabhas <abhas123mnm@gmail.com>
2024-04-18 16:06:28 +0545

    feat: admin component for managing batches

---
+++ client/src/components/dashboardComponents/AdminComponents/ManageBatch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const ManageBatch = () => {
+  return (
+    <main>
+      <Typography variant="h6">Manage Batch</Typography>
+    </main>
+  );
+};
+
+export default ManageBatch;
