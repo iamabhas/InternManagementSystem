5c94de8cd11e4803f8f702e2147c88dd9f746d0a
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-04-19 16:44:04 +0545

    fix: fix login services bugs

--- server/src/services/login.service.ts
+++ server/src/services/login.service.ts
@@ -6,36 +6,26 @@ import { IUserRequestBody } from "../@types/interface/RequestBody";
 import { AccessToken } from "../utils/jwtUtils/token";
 import { handleFourStatusError } from "../utils/Errors/CommonFourResponseError";
 import user from "../database/schema/user.schema";
+import bcryptjs from "bcrypt";
 const { ERROR, FAIL, SUCCESS } = statusConstants;
-const { ADMIN, USER, SUPER_ADMIN } = roleConstants;
+const { ADMIN, USER, MENTOR, SUPER_ADMIN } = roleConstants;
 
 export const loginService = async (
   username: string,
   password: string,
   res: Response
 ): Promise<any> => {
-  // return async (_req: Request, res: Response) => {
   if ((!username && username === null) || (!password && password === null)) {
     return res.status(404).json({
       error: FAIL,
       message: "Missing Credentials, Email and Password is Not Provided",
     });
   }
-
-  //dont delete it is for register purpose for later
-  // if (!isStrongPassword(password)) {
-  //   return handleFourStatusError(
-  //     res,
-  //     404,
-  //     FAIL,
-  //     "PassWord is Not Strong , Please Include LowerCase , UpperCase and a Digit"
-  //   );
-  // }
-
   await userNameValidation(username)
     .then(async (response: boolean): Promise<any> => {
       const User = await user.findOne({ username: username });
       console.log(User);
+
       if (
         !User ||
         (await user.find({ username: username }).countDocuments()) === 0
@@ -47,33 +37,18 @@ export const loginService = async (
           "Email Not Found , Please Contact Super Admin"
         );
       }
-
-      // const UserRole : Pick<IUserRequestBody, "role"> = User;
-      const Roles: string[] = [ADMIN, USER, SUPER_ADMIN];
+      const Roles: string[] = [MENTOR, USER, ADMIN, SUPER_ADMIN];
       const UserRole: string | Partial<IUserRequestBody> = User.role;
-
+      const checkPassword = await bcryptjs.compare(password, User.password);
       if (Roles.includes(UserRole)) {
-        if (
-          User.password.includes(password) &&
-          User.password.split(" ").includes(password)
-        ) {
+        console.log(checkPassword);
+        if (checkPassword && typeof checkPassword === "boolean") {
           const accesstoken = await AccessToken(
             User._id,
             User.username,
             User.role
           );
-
-          // return sendTokenResponse(
-          //   res,
-          //   200,
-          //   response,
-          //   SUCCESS,
-          //   accesstoken,
-          //   User.username,
-          //   User.role,
-          //   User.id
-          // );
-          return res.status(204).json({
+          return res.status(203).json({
             Status: response,
             error: SUCCESS,
             message: "Access Token Generated and set In Headers",
