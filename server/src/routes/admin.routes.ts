56016ce7e677da0d42012b38d3adad772b3c36da
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-04-25 15:31:32 +0545

    Added route to fetch user details by ID

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,10 +1,14 @@
 import e, { Router } from "express";
-import adminController from "../controller/admin.controller";
+import express from 'express' ;
+import { createBatch } from "../controller/admin.controller";
+import { getUserById } from "../controller/admin.controller";
 import { roleConstants } from "../constants/roleConstants";
 import { BatchController } from "../controller/batch.controller";
 import { validateToken } from "../middleware/apiAuth.middleware";
 import { validateRole } from "../middleware/checkRole.middleware";
 import { restrictRole } from "../middleware/RestrictRoles";
+
+
 const { USER, ADMIN, MENTOR } = roleConstants;
 const adminRouter = Router();
 
@@ -13,55 +17,59 @@ adminRouter.post(
   validateToken,
   validateRole,
   restrictRole(USER, MENTOR),
-  adminController.createBatch
+  createBatch
 );
 
-adminRouter.post(
-  "/batch/intern",
-  validateToken,
-  validateRole,
-  restrictRole(USER, MENTOR),
-  adminController.registerInterns
-);
+// adminRouter.post(
+//   "/batch/intern",
+//   validateToken,
+//   validateRole,
+//   restrictRole(USER, MENTOR),
+//   adminController.registerInterns
+// );
 
-adminRouter.post(
-  "/batch/mentor",
-  validateToken,
-  validateRole,
-  restrictRole(USER, MENTOR),
-  adminController.registerMentors
-);
+// adminRouter.post(
+// //   "/batch/mentor",
+// //   validateToken,
+// //   validateRole,
+// //   restrictRole(USER, MENTOR),
+// //   adminController.registerMentors
+// // );
 
-adminRouter.get(
-  "/batch",
-  validateToken,
-  validateRole,
-  restrictRole(USER, MENTOR),
-  BatchController.getBatchController
-);
+// adminRouter.get(
+//   "/batch",
+//   validateToken,
+//   validateRole,
+//   restrictRole(USER, MENTOR),
+//  g
+// );
 
-adminRouter.get(
-  "/batch/:id",
-  validateToken,
-  validateRole,
-  restrictRole(USER, MENTOR),
-  BatchController.getBatchByIdController
-);
+// adminRouter.get(
+//   "/batch/:id",
+//   validateToken,
+//   validateRole,
+//   restrictRole(USER, MENTOR),
+//   BatchController.getBatchByIdController
+// );
 
-adminRouter.get(
-  "/batchintern",
-  validateToken,
-  validateRole,
-  restrictRole(USER),
-  BatchController.getAllIntern
-);
+// adminRouter.get(
+//   "/batchintern",
+//   validateToken,
+//   validateRole,
+//   restrictRole(USER),
+//   BatchController.getAllIntern
+// );
 
-adminRouter.get(
-  "/batch/intern/:id",
-  validateToken,
-  validateRole,
-  restrictRole(USER, MENTOR),
-  BatchController.getInternById
-);
+// adminRouter.get(
+//   "/batch/intern/:id",
+//   validateToken,
+//   validateRole,
+//   restrictRole(USER, MENTOR),
+//   BatchController.getInternById
+// );
 
-export default adminRouter;
+
+const router = express.Router ();
+router.get('/users/:id' , getUserById);
+
+export default { router , adminRouter } ;
