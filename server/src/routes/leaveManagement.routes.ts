f71c99db9f889ec618b99525c9bd70f3683e7d00
iamabhas <abhas123mnm@gmail.com>
2024-04-25 08:08:16 +0545

    feat: added leave Application Management routes

---
+++ server/src/routes/leaveManagement.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import leaveApplicationController from "../controller/leaveApplication.controller";
+import { roleConstants } from "../constants/roleConstants";
+import { validateToken } from "../middleware/apiAuth.middleware";
+import { validateRole } from "../middleware/checkRole.middleware";
+import { restrictRole } from "../middleware/RestrictRoles";
+
+const { ADMIN, MENTOR, USER } = roleConstants;
+
+const leaveManagementRouter = Router();
+
+leaveManagementRouter.post(
+  "/send-leave-application",
+  // validateToken,
+  // validateRole,
+  // restrictRole(ADMIN, MENTOR),
+  leaveApplicationController.createLeaveApplicationController
+);
+
+leaveManagementRouter.get(
+  "/get-leave-applications",
+  // validateToken,
+  // validateRole,
+  // restrictRole(USER, MENTOR)
+  leaveApplicationController.getApplications
+);
+
+export default leaveManagementRouter;
