16fbb7cf87aa62e20125a06905f8468a0233e2ad
iamabhas <abhas123mnm@gmail.com>
2024-04-26 15:52:08 +0545

    feat: intern qualification schema

---
+++ server/src/database/schema/internQualification.schema.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { fieldCannotBeEmpty } from "../../utils/schemaUtils/userSchemaMessage";
+
+const internQualification = new mongoose.Schema({
+  universityName: {
+    type: String,
+    required: [true, fieldCannotBeEmpty("universityName")],
+  },
+  graduationYear: {
+    type: Number,
+    min: 4,
+    max: 4,
+    required: [true, fieldCannotBeEmpty("graduationYear")],
+  },
+  graduationMonth: {
+    type: Number,
+    min: 1,
+    max: 2,
+    required: [true, fieldCannotBeEmpty("graduationMonth")],
+  },
+  skills: {
+    type: String,
+    required: [true, fieldCannotBeEmpty("skills")],
+  },
+  Intern: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+});
+
+const InternQualification = mongoose.model(
+  "internQualification",
+  internQualification
+);
+
+export default InternQualification;
