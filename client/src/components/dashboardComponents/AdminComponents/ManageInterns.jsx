2d7bec49f0078f6ef06aead58481b14cc1604302
iamabhas <abhas123mnm@gmail.com>
2024-04-29 15:26:11 +0545

    refactor: formatting changed

--- client/src/components/dashboardComponents/AdminComponents/ManageInterns.jsx
+++ client/src/components/dashboardComponents/AdminComponents/ManageInterns.jsx
@@ -1,329 +1,296 @@
 import * as React from "react";
 import {
-  Button,
-  TextField,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Box,
-  Typography,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TablePagination,
-  LinearProgress,
-  Grid,
+    Button,
+    TextField,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Box,
+    Typography,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TablePagination,
+    Grid,
 } from "@mui/material";
 
-import PropTypes from "prop-types";
 import Swal from "sweetalert2";
-import { IoMdAddCircle } from "react-icons/io";
-import { registerIntern } from "../../../services/Api";
-import { IoFilter } from "react-icons/io5";
+import {IoMdAddCircle} from "react-icons/io";
+import {registerIntern} from "../../../services/Api";
+import {IoFilter} from "react-icons/io5";
 import axios from "axios";
-import { useNavigate } from "react-router";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
-function LinearProgressWithLabel(props) {
-  return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Box sx={{ width: "100%", mr: 1 }}>
-        <LinearProgress variant="determinate" {...props} />
-      </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">{`${Math.round(
-          props.value
-        )}%`}</Typography>
-      </Box>
-    </Box>
-  );
-}
-
-LinearProgressWithLabel.propTypes = {
-  value: PropTypes.number.isRequired,
-};
 
 export default function ManageInterns() {
-  const [open, setOpen] = React.useState(false);
-  const [data, setData] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const accesstoken = useSelector((state) => state.auth.token);
+    const [open, setOpen] = React.useState(false);
+    const [data, setData] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const accesstoken = useSelector((state) => state.auth.token);
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/batchintern",
-          {
-            headers: {
-              Authorization: accesstoken,
-            },
-          }
-        );
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:5000/api/batchintern",
+                    {
+                        headers: {
+                            Authorization: accesstoken,
+                        },
+                    }
+                );
 
-        setData(response.data.data);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
-    };
-    fetchData();
-  }, []);
+                setData(response.data.data);
+            } catch (error) {
+                console.error("Error fetching data:", error.message);
+            }
+        };
+        fetchData();
+    }, []);
 
-  const [inputs, setInputs] = React.useState({
-    username: "",
-    fullname: "",
-    email: "",
-    phoneNo: "",
-    role: "",
-  });
-
-  const handleChange = (e) => {
-    setInputs((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const userData = await registerIntern(inputs);
-      if (userData !== null) {
-        Swal.fire({
-          icon: "success",
-          title: "Success",
-          text: "Intern Register SuccessFully",
-        });
-      }
-    } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Error",
-        text: error.message || "Error",
-      });
-    }
-  };
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setInputs({
-      username: "",
-      fullname: "",
-      email: "",
-      phoneNo: "",
-      role: "",
+    const [inputs, setInputs] = React.useState({
+        username: "",
+        fullname: "",
+        email: "",
+        phoneNo: "",
+        role: "",
     });
-  };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+    const handleChange = (e) => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const userData = await registerIntern(inputs);
+            if (userData !== null) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Success",
+                    text: "Intern Register SuccessFully",
+                });
+            }
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: error.message || "Error",
+            });
+        }
+    };
 
-  const nobatch = "Not In A Batch";
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  return (
-    <React.Fragment>
-      <Typography variant="h4" sx={{ m: 2 }} style={{ textAlign: "center" }}>
-        Manage Intern
-      </Typography>
-      <Box textAlign="center" sx={{ m: 4 }}>
-        {" "}
-        <Button
-          variant="outlined"
-          onClick={handleClickOpen}
-          color="success"
-          startIcon={<IoMdAddCircle />}
-        >
-          Add Intern
-        </Button>
-        <Button
-          variant="outlined"
-          color="warning"
-          sx={{ m: 1 }}
-          startIcon={<IoFilter />}
-        >
-          Filter Interns
-        </Button>
-      </Box>
+    const handleClose = () => {
+        setOpen(false);
+        setInputs({
+            username: "",
+            fullname: "",
+            email: "",
+            phoneNo: "",
+            role: "",
+        });
+    };
 
-      <Box textAlign="center" sx={{ m: 5 }}>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Full Name</TableCell>
-                <TableCell align="center">User Name</TableCell>
-                <TableCell align="center">Phone No</TableCell>
-                <TableCell align="center">Email</TableCell>
-                <TableCell align="center">Batch</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {[...data]
-                .reverse()
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => (
-                  <TableRow
-                    key={row.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.fullname}
-                    </TableCell>
-                    <TableCell align="center">{row.fullname}</TableCell>
-                    <TableCell align="center">{row.phoneNo}</TableCell>
-                    <TableCell align="center">{row.email}</TableCell>
-                    <TableCell align="center">{row.Batch.name}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 15]}
-          component="div"
-          count={data.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Box>
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
 
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: "form",
-          onSubmit: (event) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries(formData.entries());
-            const email = formJson.email;
-            handleClose();
-          },
-        }}
-        fullWidth
-      >
-        <DialogTitle>Add Intern</DialogTitle>
-        <DialogContent>
-          <Grid container spacing={2}>
-            {" "}
-            {/* Grid container */}
-            <Grid item xs={12} md={6}>
-              {" "}
-              {/* Grid item for Intern Name */}
-              <TextField
-                margin="normal"
-                required
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <React.Fragment>
+            <Typography variant="h4" sx={{m: 2}} style={{textAlign: "center"}}>
+                Manage Intern
+            </Typography>
+            <Box textAlign="center" sx={{m: 4}}>
+                {" "}
+                <Button
+                    variant="outlined"
+                    onClick={handleClickOpen}
+                    color="success"
+                    startIcon={<IoMdAddCircle/>}
+                >
+                    Add Intern
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="warning"
+                    sx={{m: 1}}
+                    startIcon={<IoFilter/>}
+                >
+                    Filter Interns
+                </Button>
+            </Box>
+
+            <Box textAlign="center" sx={{m: 5}}>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Full Name</TableCell>
+                                <TableCell align="center">User Name</TableCell>
+                                <TableCell align="center">Phone No</TableCell>
+                                <TableCell align="center">Email</TableCell>
+                                <TableCell align="center">Batch</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {[...data]
+                                .reverse()
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        sx={{"&:last-child td, &:last-child th": {border: 0}}}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.fullname}
+                                        </TableCell>
+                                        <TableCell align="center">{row.fullname}</TableCell>
+                                        <TableCell align="center">{row.phoneNo}</TableCell>
+                                        <TableCell align="center">{row.email}</TableCell>
+                                        <TableCell align="center">{row.Batch.name}</TableCell>
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 15]}
+                    component="div"
+                    count={data.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Box>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
                 fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-                value={inputs.username}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              {" "}
-              {/* Grid item for Intern Full Name */}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="fullname"
-                label="Fullname"
-                name="fullname"
-                autoComplete="fullname"
-                autoFocus
-                value={inputs.fullname}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              {" "}
-              {/* Grid item for Intern Email */}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={inputs.email}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              {" "}
-              {/* Grid item for Role */}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="role"
-                label="Role"
-                name="role"
-                autoComplete="role"
-                autoFocus
-                value={inputs.role}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              {" "}
-              {/* Grid item for Intern Phone Number */}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="phoneNo"
-                label="Phone Number"
-                name="phoneNo"
-                autoComplete="phoneNumber"
-                autoFocus
-                value={inputs.phoneNo}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-        </DialogContent>
+            >
+                <DialogTitle>Add Intern</DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={2}>
+                        {" "}
+                        {/* Grid container */}
+                        <Grid item xs={12} md={6}>
+                            {" "}
+                            {/* Grid item for Intern Name */}
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                autoComplete="username"
+                                autoFocus
+                                value={inputs.username}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            {" "}
+                            {/* Grid item for Intern Full Name */}
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="fullname"
+                                label="Fullname"
+                                name="fullname"
+                                autoComplete="fullname"
+                                autoFocus
+                                value={inputs.fullname}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            {" "}
+                            {/* Grid item for Intern Email */}
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                value={inputs.email}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            {" "}
+                            {/* Grid item for Role */}
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="role"
+                                label="Role"
+                                name="role"
+                                autoComplete="role"
+                                autoFocus
+                                value={inputs.role}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            {" "}
+                            {/* Grid item for Intern Phone Number */}
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="phoneNo"
+                                label="Phone Number"
+                                name="phoneNo"
+                                autoComplete="phoneNumber"
+                                autoFocus
+                                value={inputs.phoneNo}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
 
-        <Box textAlign="center" sx={{ m: 1 }}>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => handleSubmit}
-          >
-            Add Intern
-          </Button>
-        </Box>
+                <Box textAlign="center" sx={{m: 1}}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => handleSubmit}
+                    >
+                        Add Intern
+                    </Button>
+                </Box>
 
-        <DialogActions>
-          <Button onClick={handleClose} color="error">
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
-  );
+                <DialogActions>
+                    <Button onClick={handleClose} color="error">
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
 }
