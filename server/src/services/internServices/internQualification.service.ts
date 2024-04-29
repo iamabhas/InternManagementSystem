6f54dc40687b31333fb9d29bd7f96dfb6a5ccce4
iamabhas <abhas123mnm@gmail.com>
2024-04-29 16:04:59 +0545

    TODO list

---
+++ server/src/services/internServices/internQualification.service.ts
@@ -0,0 +1,9 @@
+// Add | Edit Qualifications ( Interns )
+
+// Get Intern Qualifications according to batch id (Admin)
+
+// Get Intern Qualifications according to user id ( Admins )
+
+// Export Intern Qualifications as xslx according to batch id
+
+
