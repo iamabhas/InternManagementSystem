b2e965187d30cd306bd0a0c784434306dc5cc869
iamabhas <abhas123mnm@gmail.com>
2024-04-29 16:04:49 +0545

    refactor: added Batch reference in schema

--- server/src/database/schema/internQualification.schema.ts
+++ server/src/database/schema/internQualification.schema.ts
@@ -1,38 +1,44 @@
 import mongoose from "mongoose";
-import { fieldCannotBeEmpty } from "../../utils/schemaUtils/userSchemaMessage";
+import {fieldCannotBeEmpty} from "../../utils/schemaUtils/userSchemaMessage";
 
 const internQualification = new mongoose.Schema({
-  universityName: {
-    type: String,
-    required: [true, fieldCannotBeEmpty("universityName")],
-  },
-  graduationYear: {
-    type: Number,
-    min: 4,
-    max: 4,
-    required: [true, fieldCannotBeEmpty("graduationYear")],
-  },
-  graduationMonth: {
-    type: Number,
-    min: 1,
-    max: 2,
-    required: [true, fieldCannotBeEmpty("graduationMonth")],
-  },
-  skills: {
-    type: String,
-    required: [true, fieldCannotBeEmpty("skills")],
-  },
-  Intern: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
+    universityName: {
+        type: String,
+        required: [true, fieldCannotBeEmpty("universityName")],
     },
-  ],
+    graduationYear: {
+        type: Number,
+        min: 4,
+        max: 4,
+        required: [true, fieldCannotBeEmpty("graduationYear")],
+    },
+    graduationMonth: {
+        type: Number,
+        min: 1,
+        max: 2,
+        required: [true, fieldCannotBeEmpty("graduationMonth")],
+    },
+    skills: {
+        type: String,
+        required: [true, fieldCannotBeEmpty("skills")],
+    },
+    Intern: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        },
+    ],
+    Batch: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "batch",
+        },
+    ],
 });
 
 const InternQualification = mongoose.model(
-  "internQualification",
-  internQualification
+    "internQualification",
+    internQualification
 );
 
 export default InternQualification;
