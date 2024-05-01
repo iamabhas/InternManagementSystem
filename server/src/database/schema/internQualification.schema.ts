b01733bcf3d2850a3c4b72a1977da1c96564ba32
iamabhas <abhas123mnm@gmail.com>
2024-05-01 11:21:15 +0545

    refactor: fields arent set to required

--- server/src/database/schema/internQualification.schema.ts
+++ server/src/database/schema/internQualification.schema.ts
@@ -1,44 +1,38 @@
 import mongoose from "mongoose";
-import {fieldCannotBeEmpty} from "../../utils/schemaUtils/userSchemaMessage";
 
 const internQualification = new mongoose.Schema({
-    universityName: {
-        type: String,
-        required: [true, fieldCannotBeEmpty("universityName")],
-        default: "Not Filled Yet"
-    },
-    graduationYear: {
-        type: Number,
-        required: [true, fieldCannotBeEmpty("graduationYear")],
-        default: 0
-    },
-    graduationMonth: {
-        type: Number,
-        min: 1,
-        max: 12,
-        required: [true, fieldCannotBeEmpty("graduationMonth")],
-        default: 0
-    },
-    skills: {
-        type: String,
-        required: [true, fieldCannotBeEmpty("skills")],
-        default: "Unspecified"
-    },
-    Intern: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-    },
+  universityName: {
+    type: String,
+    default: "Not Filled Yet",
+  },
+  graduationYear: {
+    type: Number,
+    default: 0,
+  },
+  graduationMonth: {
+    type: Number,
+    min: 1,
+    max: 12,
+    default: 0,
+  },
+  skills: {
+    type: String,
+    default: "Unspecified",
+  },
+  Intern: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
 
-    Batch: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "batch",
-    },
-
+  Batch: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "batch",
+  },
 });
 
 const InternQualification = mongoose.model(
-    "internQualification",
-    internQualification
+  "internQualification",
+  internQualification
 );
 
 export default InternQualification;
