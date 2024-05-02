e92df00fc40da110e582e7ab246a84b786c7fc03
iamabhas <abhas123mnm@gmail.com>
2024-05-02 10:12:10 +0545

    refactor: changed imports and formatting

--- server/src/@types/interface/RequestBody.ts
+++ server/src/@types/interface/RequestBody.ts
@@ -1,35 +1,37 @@
 import mongoose from "mongoose";
-import { NextFunction } from "express";
+import {NextFunction} from "express";
+
 export interface IUserRequestBody {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
 
 export interface IUserRole extends IUserRequestBody {
-  role: string;
+    role: string;
 }
 
 export type IOptions = {
-  issuer: string;
-  expiresIn: Date;
+    issuer: string;
+    expiresIn: Date;
 };
 
 export interface ICookie {
-  path?: string;
-  domain?: string;
-  maxAge?: number;
-  expires?: Date;
-  secure?: boolean;
-  httpOnly?: boolean;
-  sameSite?: boolean | "strict" | "lax" | "none"; // Adjusted sameSite property
+    path?: string;
+    domain?: string;
+    maxAge?: number;
+    expires?: Date;
+    secure?: boolean;
+    httpOnly?: boolean;
+    sameSite?: boolean | "strict" | "lax" | "none"; // Adjusted sameSite property
 }
+
 export type functionReq = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => void;
 
-export type TdefaultOptions = {
-  format: string;
-  printBackground: boolean;
+export type IDefaultOptions = {
+    format: string;
+    printBackground: boolean;
 };
