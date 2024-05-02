dbf09738208202eebc64cd0f3e2ef25740175c26
iamabhas <abhas123mnm@gmail.com>
2024-05-02 15:07:43 +0545

    feat: register qualifications component added

---
+++ client/src/components/dashboardComponents/InternComponents/RegisterQualifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterQualifications = () => {
+    return (
+        <h1>
+            Register Qualifications
+        </h1>
+    );
+};
+
+export default RegisterQualifications;
