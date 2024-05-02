f735b9ddf1fd7fdaeb599cb6e146342549a46077
iamabhas <abhas123mnm@gmail.com>
2024-05-02 12:15:15 +0545

    refactor: renamed from jsx to js

--- client/src/utils/GenerateColors.jsx
+++
@@ -1,5 +0,0 @@
-import { schemeCategory10 } from "d3-scale-chromatic";
-export const generateColors = (data) => {
-  const colorScale = schemeCategory10;
-  return data.map((entry, index) => colorScale[index % colorScale.length]);
-};
