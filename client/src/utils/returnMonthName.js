59df2397399c76adaa3db82bb82ab98b054e992f
iamabhas <abhas123mnm@gmail.com>
2024-05-03 12:09:33 +0545

    feat: function returns month name for month num

---
+++ client/src/utils/returnMonthName.js
@@ -0,0 +1,7 @@
+var months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+export const returnMonthName = (monthNum) => {
+    return months[monthNum]
+}
