4d61867169d0e35b1c9c18d06365a20c3bfc52f5
iamabhas <abhas123mnm@gmail.com>
2024-05-06 10:54:34 +0545

    refactor: added fields in forms and validation

--- client/src/components/dashboardComponents/InternComponents/RegisterQualifications.jsx
+++ client/src/components/dashboardComponents/InternComponents/RegisterQualifications.jsx
@@ -19,7 +19,9 @@ const RegisterQualifications = () => {
     } = useForm({
         defaultValues: {
             universityName: "",
-            skills: ""
+            skills: "",
+            graduationMonth: "",
+            graduationYear: "",
         },
         mode: "onBlur",
     });
@@ -32,7 +34,10 @@ const RegisterQualifications = () => {
         setOpen(false);
         reset({
             universityName: "",
-            skills: ""
+            skills: "",
+            graduationMonth: "",
+            graduationYear: "",
+
         })
     };
 
@@ -58,8 +63,8 @@ const RegisterQualifications = () => {
 
                         component="form" noValidate onSubmit={handleSubmit(onSubmit)}
                     >
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={12}>
+                        <Grid container spacing={2} sx={{p: 2}}>
+                            <Grid item xs={12} sm={6}>
                                 <Controller
                                     name="universityName"
                                     control={control}
@@ -84,7 +89,7 @@ const RegisterQualifications = () => {
                                     )}
                                 />
                             </Grid>
-                            <Grid item xs={12} sm={12}>
+                            <Grid item xs={12} sm={6}>
                                 <Controller
                                     name="skills"
                                     control={control}
@@ -108,21 +113,78 @@ const RegisterQualifications = () => {
                                     )}
                                 />
                             </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Controller
+                                    name="graduationMonth"
+                                    control={control}
+                                    rules={{
+                                        required: "Graduation Month is required", pattern: {
+                                            value: /^(1[0-2]|[1-9])$/,
+                                            message: "Graduation month should be a valid month"
+                                        }
+                                    }}
+                                    render={({field}) => (
+                                        <TextField
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            type="number"
+                                            id="graduationMonth"
+                                            label="Graduation Month"
+                                            autoComplete="graduationMonth"
+                                            error={!!errors.graduationMonth}
+                                            helperText={
+                                                errors.graduationMonth
+                                                    ? errors.graduationMonth.message
+                                                    : ""
+                                            }
+                                            {...field}
+                                        />
+                                    )}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Controller
+                                    name="graduationYear"
+                                    control={control}
+                                    rules={{
+                                        required: "Graduation Year is required", pattern: {
+                                            value: /^\d{4}$/,
+                                            message: "Graduation year should be a valid year"
+                                        }
+                                    }}
+                                    render={({field}) => (
+                                        <TextField
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            id="graduationYear"
+                                            label="Graduation Year"
+                                            type="number"
+                                            autoComplete="graduationYear"
+                                            error={!!errors.graduationYear}
+                                            helperText={
+                                                errors.graduationYear
+                                                    ? errors.graduationYear.message
+                                                    : ""
+                                            }
+                                            {...field}
+                                        />
+                                    )}
+                                />
+                            </Grid>
 
                         </Grid>
                         <Button
                             type="submit"
-                            fullWidth
                             variant="contained"
-                            sx={{mt: 3, mb: 2}}
+                            sx={{m: 2}}
                             disabled={!isValid}
 
                         >
                             Add Qualifications
                         </Button>
                     </Box>
-
-
                 </Box>
             </Dialog>
         </div>
