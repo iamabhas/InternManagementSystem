f96117ebb67e00e352de39093cb9b38de2f486aa
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-05-06 18:00:51 +0545

    feat: add leave history for particular interns

--- client/src/components/dashboardComponents/InternComponents/SendLeave.jsx
+++ client/src/components/dashboardComponents/InternComponents/SendLeave.jsx
@@ -1,196 +1,248 @@
 import React from "react";
-import {Controller, useForm} from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import {
-    Box,
-    Button,
-    Dialog,
-    DialogTitle,
-    TextField,
-    Grid,
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  TextField,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
 } from "@mui/material";
-import {RiMailSendLine} from "react-icons/ri";
+import { RiMailSendLine } from "react-icons/ri";
 import axios from "axios";
-// Date imports
-import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import {useSelector} from "react-redux";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useSelector } from "react-redux";
+import { formatDate } from "../../../utils/dateFormatter";
 
 const SendLeave = () => {
-    const [open, setOpen] = React.useState(false);
-    const [leavesData, setLeavesData] = React.useState([]);
-    const accesstoken = useSelector((state) => state.auth.token);
-    const [dateInputs, setDateInputs] = React.useState({
-        leaveFromDate: null,
-        leaveToDate: null,
-    });
+  const [open, setOpen] = React.useState(false);
+  const [leavesData, setLeavesData] = React.useState([]);
+  const accessToken = useSelector((state) => state.auth.token);
+  const [dateInputs, setDateInputs] = React.useState({
+    leaveFromDate: null,
+    leaveToDate: null,
+  });
 
-    const {
-        control,
-        handleSubmit,
-        reset,
-        formState: {errors, isValid},
-    } = useForm({
-        defaultValues: {
-            subject: "",
-            applicationBody: "",
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      subject: "",
+      applicationBody: "",
+    },
+    mode: "onBlur",
+  });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/leave-intern-all",
+          {
+            headers: {
+              Authorization: accessToken,
+            },
+          }
+        );
+        setLeavesData(response.data.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchData();
+  }, [accessToken]);
+
+  const handleClose = () => {
+    setOpen(false);
+    reset({
+      subject: "",
+      applicationBody: "",
+    });
+    setDateInputs({
+      leaveFromDate: null,
+      leaveToDate: null,
+    });
+  };
+
+  const handleDateChange = (key, date) => {
+    setDateInputs((prev) => ({
+      ...prev,
+      [key]: date,
+    }));
+  };
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/send-leave-application",
+        {
+          ...data,
+          leaveFromDate: dateInputs.leaveFromDate,
+          leaveToDate: dateInputs.leaveToDate,
         },
-        mode: "onBlur",
-    });
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-        reset({
-            subject: "",
-            applicationBody: "",
-        });
-        setDateInputs({
-            leaveFromDate: null,
-            leaveToDate: null,
-        });
-    };
-
-    const handleDateChange = (key, date) => {
-        setDateInputs((prev) => ({
-            ...prev,
-            [key]: date,
-        }));
-    };
-
-    const onSubmit = async (data) => {
-        const requestData = {
-            ...data,
-            leaveFromDate: dateInputs.leaveFromDate,
-            leaveToDate: dateInputs.leaveToDate,
-        };
-
-        try {
-            const response = await axios.post(
-                "http://localhost:5000/api/send-leave-application",
-                requestData,
-                {
-                    headers: {
-                        Authorization: accesstoken,
-                    },
-                }
-            );
-            console.log(response.data);
-        } catch (error) {
-            console.log(error);
-            console.error(error.message);
-        } finally {
-            handleClose();
+        {
+          headers: {
+            Authorization: accessToken,
+          },
         }
-    };
+      );
+      setLeavesData([...leavesData, response.data.data]);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      handleClose();
+    }
+  };
 
-    const checkDateValidation = () => {
-        if (!dateInputs.leaveToDate || !dateInputs.leaveFromDate) {
-            return true;
-        }
-    };
+  const checkDateValidation = () => {
+    return !dateInputs.leaveToDate || !dateInputs.leaveFromDate;
+  };
 
-    return (
-        <div>
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        endIcon={<RiMailSendLine />}
+        onClick={handleClickOpen}
+        color="success"
+        sx={{ marginBottom: 2 }}
+      >
+        Send Leave Application
+      </Button>
+      <Dialog open={open} onClose={handleClose} fullWidth>
+        <DialogTitle>Leave Application</DialogTitle>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: 2,
+          }}
+        >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Controller
+                  name="subject"
+                  control={control}
+                  rules={{ required: "Application Subject is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      label="Subject"
+                      variant="outlined"
+                      fullWidth
+                      autoFocus
+                      error={!!errors.subject}
+                      helperText={errors.subject ? errors.subject.message : ""}
+                      {...field}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Controller
+                  name="applicationBody"
+                  control={control}
+                  rules={{ required: "Application Body is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      label="Body"
+                      variant="outlined"
+                      fullWidth
+                      multiline
+                      rows={4}
+                      error={!!errors.applicationBody}
+                      helperText={
+                        errors.applicationBody
+                          ? errors.applicationBody.message
+                          : ""
+                      }
+                      {...field}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="Start of Leave"
+                    fullWidth
+                    value={dateInputs.leaveFromDate}
+                    onChange={(date) => handleDateChange("leaveFromDate", date)}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="End of Leave"
+                    fullWidth
+                    value={dateInputs.leaveToDate}
+                    onChange={(date) => handleDateChange("leaveToDate", date)}
+                  />
+                </LocalizationProvider>
+              </Grid>
+            </Grid>
             <Button
-                variant="outlined"
-                endIcon={<RiMailSendLine/>}
-                onClick={handleClickOpen}
-                color="success"
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={!isValid || checkDateValidation()}
+              sx={{ mt: 2 }}
             >
-                Send Leave Application
+              Send Leave
             </Button>
-            <Dialog open={open} onClose={handleClose} fullWidth>
-                <DialogTitle>Leave Application</DialogTitle>
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)}>
-                        <Grid container spacing={2} sx={{p: 2}}>
-                            <Grid item xs={12} sm={12}>
-                                <Controller
-                                    name="subject"
-                                    control={control}
-                                    rules={{required: "Application Subject is required"}}
-                                    render={({field}) => (
-                                        <TextField
-                                            margin="normal"
-                                            required
-                                            fullWidth
-                                            id="subject"
-                                            label="Subject"
-                                            autoComplete="subject"
-                                            autoFocus
-                                            error={!!errors.subject}
-                                            helperText={errors.subject ? errors.subject.message : ""}
-                                            {...field}
-                                        />
-                                    )}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={12}>
-                                <Controller
-                                    name="applicationBody"
-                                    control={control}
-                                    rules={{required: "Application Body is required"}}
-                                    render={({field}) => (
-                                        <TextField
-                                            margin="normal"
-                                            required
-                                            fullWidth
-                                            id="applicationBody"
-                                            label="Body"
-                                            autoComplete="applicationBody"
-                                            error={!!errors.applicationBody}
-                                            helperText={
-                                                errors.applicationBody
-                                                    ? errors.applicationBody.message
-                                                    : ""
-                                            }
-                                            {...field}
-                                        />
-                                    )}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={12}>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DatePicker
-                                        label="Start of Leave"
-                                        sx={{m: 1}}
-                                        fullWidth
-                                        value={dateInputs.leaveFromDate}
-                                        onChange={(date) => handleDateChange("leaveFromDate", date)}
-                                    />
+          </form>
+        </Box>
+      </Dialog>
 
-                                    <DatePicker
-                                        label="End of Leave"
-                                        sx={{m: 1}}
-                                        fullWidth
-                                        value={dateInputs.leaveToDate}
-                                        onChange={(date) => handleDateChange("leaveToDate", date)}
-                                    />
-                                </LocalizationProvider>
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            sx={{m: 2}}
-                            disabled={!isValid || checkDateValidation()}
-                        >
-                            Send Leave
-                        </Button>
-                    </Box>
-                </Box>
-            </Dialog>
-        </div>
-    );
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="h5" gutterBottom>
+          Leave History
+        </Typography>
+        <TableContainer component={Paper} sx={{ borderRadius: 4 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Subject</TableCell>
+                <TableCell>Start Date</TableCell>
+                <TableCell>End Date</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {leavesData.map((leave) => (
+                <TableRow key={leave.id}>
+                  <TableCell>{leave.subject}</TableCell>
+                  <TableCell>{formatDate(leave.leaveFromDate)}</TableCell>
+                  <TableCell>{formatDate(leave.leaveToDate)}</TableCell>
+                  <TableCell>
+                    {leave.approveStatus ? "Approved" : "Pending"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </div>
+  );
 };
 
 export default SendLeave;
