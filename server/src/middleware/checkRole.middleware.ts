2a1697e0ae2e5aa58dde4267f1ed7d4e129088c9
iamabhas <abhas123mnm@gmail.com>
2024-05-06 07:49:28 +0545

    refactor: formatting changes

--- server/src/middleware/checkRole.middleware.ts
+++ server/src/middleware/checkRole.middleware.ts
@@ -1,31 +1,24 @@
-import { Request, Response, NextFunction } from "express";
-
-import { roleConstants } from "../constants/roleConstants";
-import { statusConstants } from "../constants/statusConstants";
+import {Request, Response, NextFunction} from "express";
+import {roleConstants} from "../constants/roleConstants";
 import AppError from "../utils/errorUtils/appError";
-const { ADMIN, USER, SUPER_ADMIN, MENTOR } = roleConstants;
-const { FAIL, ERROR, SUCCESS } = statusConstants;
 
-const checkUser = async (role: string) => {
-  if (role === USER) {
-    return true;
-  }
-};
+const {ADMIN, USER, SUPER_ADMIN, MENTOR} = roleConstants;
+
 
 export const validateRole = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    _: Response,
+    next: NextFunction
 ) => {
-  const role: string = req.user.role;
-  if (typeof role !== "string" || role === null || undefined) {
-    throw new AppError("Role is not Invalid Type", 405);
-  }
-  const roleArray: Array<String> = [ADMIN, USER, SUPER_ADMIN, MENTOR];
+    const role: string = req.user.role;
+    if (typeof role !== "string" || role === null || undefined) {
+        throw new AppError("Role is not valid Type", 405);
+    }
+    const roleArray: Array<String> = [ADMIN, USER, SUPER_ADMIN, MENTOR];
 
-  if (roleArray.includes(role)) {
-    next();
-  } else {
-    throw new AppError(`${role} Does Not Exists`, 401);
-  }
+    if (roleArray.includes(role)) {
+        next();
+    } else {
+        throw new AppError(`${role} Does Not Exists`, 401);
+    }
 };
