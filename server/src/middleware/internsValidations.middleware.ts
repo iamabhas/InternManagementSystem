e66a6eb061486ab1ab6758e2f131c6c8705f9416
iamabhas <abhas123mnm@gmail.com>
2024-05-06 07:47:56 +0545

    FIXED: Intern validation middleware fixed and working

--- server/src/middleware/internsValidations.middleware.ts
+++ server/src/middleware/internsValidations.middleware.ts
@@ -1,14 +1,18 @@
 import {Request, NextFunction} from "express";
+import {roleConstants} from "../constants/roleConstants";
+import AppError from "../utils/errorUtils/appError";
 
-export const internValidation = () => {
-    console.log("Outside")
-    return (req: Request, _: any, next: NextFunction) => {
-        // const userId = req.user.user_id
-        // const userRole = req.user.role
-        // const paramsId = req.params.id
-        // console.log(userId, userRole, paramsId);
-        console.log("Inside")
-        next()
+const {USER} = roleConstants
+
+export const internValidation = (req: Request, _: any, next: NextFunction) => {
+    const userId = req.user.user_id
+    const userRole = req.user.role
+    const paramsId = req.params.id
+
+    if (userRole === USER) {
+        if (userId !== paramsId) {
+            next(new AppError("Can't fetch data of other interns", 400))
+        }
     }
-
+    next()
 }
