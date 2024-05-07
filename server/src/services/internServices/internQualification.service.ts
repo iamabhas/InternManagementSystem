e732b5da58e204818f6a8c6f165363cbb4d46953
iamabhas <abhas123mnm@gmail.com>
2024-05-07 12:18:12 +0545

    refactor: return statement in repsonse for edit qualifications service

--- server/src/services/internServices/internQualification.service.ts
+++ server/src/services/internServices/internQualification.service.ts
@@ -1,156 +1,156 @@
 import mongoose from "mongoose";
 import InternQualification from "../../database/schema/internQualification.schema";
-import { Response, NextFunction } from "express";
+import {Response, NextFunction} from "express";
 import AppError from "../../utils/errorUtils/appError";
 import user from "../../database/schema/user.schema";
-import { sendResponse } from "../../helpers/customResponse";
+import {sendResponse} from "../../helpers/customResponse";
 import ExcelJS from "exceljs";
 
 export class InternQualificationService {
-  public static async addQualifications(
-    res: Response,
-    userId: mongoose.Types.ObjectId | string,
-    body: any
-  ) {
-    const existUser = await user
-      .findOne({ _id: userId })
-      .populate({ path: "Batch", select: "name" });
-    const { universityName, graduationYear, graduationMonth, skills } = body;
-    const checkExistsQualification = await InternQualification.findOne({
-      Intern: userId,
-    });
-    if (checkExistsQualification !== null) {
-      await InternQualification.updateOne(
-        { _id: checkExistsQualification._id },
-        {
-          $set: {
+    public static async addQualifications(
+        res: Response,
+        userId: mongoose.Types.ObjectId | string,
+        body: any
+    ) {
+        const existUser = await user
+            .findOne({_id: userId})
+            .populate({path: "Batch", select: "name"});
+        const {universityName, graduationYear, graduationMonth, skills} = body;
+        const checkExistsQualification = await InternQualification.findOne({
+            Intern: userId,
+        });
+        if (checkExistsQualification !== null) {
+            await InternQualification.updateOne(
+                {_id: checkExistsQualification._id},
+                {
+                    $set: {
+                        universityName: universityName,
+                        graduationYear: graduationYear,
+                        graduationMonth: graduationMonth,
+                        skills: skills,
+                    },
+                },
+                {
+                    $new: true,
+                }
+            );
+            const data = await InternQualification.findOne({
+                _id: checkExistsQualification._id,
+            });
+            return sendResponse(
+                res,
+                201,
+                "Intern Qualifications Updated successfully",
+                data
+            );
+        }
+
+        const newInternQualification = {
+            Intern: userId,
+            Batch: existUser?.Batch,
             universityName: universityName,
             graduationYear: graduationYear,
             graduationMonth: graduationMonth,
             skills: skills,
-          },
-        },
-        {
-          $new: true,
+        };
+        const internQualification = new InternQualification(newInternQualification);
+
+        await internQualification.save();
+
+        sendResponse(
+            res,
+            201,
+            "Intern Qualifications added successfully",
+            internQualification
+        );
+    }
+
+    public static async getInternQualificationById(
+        res: Response,
+        userId: mongoose.Types.ObjectId | string,
+        next: NextFunction
+    ) {
+        const internQualifications = await InternQualification.find({
+            Intern: userId,
+        })
+            .populate("Intern", "username")
+            .populate("Batch", "name");
+        if (!internQualifications) {
+            return next(
+                new AppError("No qualifications found for the specified user.", 404)
+            );
         }
-      );
-      const data = await InternQualification.findOne({
-        _id: checkExistsQualification._id,
-      });
-      sendResponse(
-        res,
-        201,
-        "Intern Qualifications Updated successfully",
-        data
-      );
+        sendResponse(
+            res,
+            200,
+            "Intern Qualifications fetched successfully",
+            internQualifications
+        );
     }
 
-    const newInternQualification = {
-      Intern: userId,
-      Batch: existUser?.Batch,
-      universityName: universityName,
-      graduationYear: graduationYear,
-      graduationMonth: graduationMonth,
-      skills: skills,
-    };
-    const internQualification = new InternQualification(newInternQualification);
+    public static async downloadBatchData(
+        res: Response,
+        batchId: mongoose.Types.ObjectId | string
+    ) {
+        const qualifications = await InternQualification.find({
+            Batch: batchId,
+        }).populate("Intern", "-_id username");
 
-    await internQualification.save();
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet("Batch Qualifications");
 
-    sendResponse(
-      res,
-      201,
-      "Intern Qualifications added successfully",
-      internQualification
-    );
-  }
+        res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+        res.setHeader(
+            "Content-Disposition",
+            `attachment; filename="batchData.xlsx"`
+        );
 
-  public static async getInternQualificationById(
-    res: Response,
-    userId: mongoose.Types.ObjectId | string,
-    next: NextFunction
-  ) {
-    const internQualifications = await InternQualification.find({
-      Intern: userId,
-    })
-      .populate("Intern", "username")
-      .populate("Batch", "name");
-    if (!internQualifications) {
-      return next(
-        new AppError("No qualifications found for the specified user.", 404)
-      );
-    }
-    sendResponse(
-      res,
-      200,
-      "Intern Qualifications fetched successfully",
-      internQualifications
-    );
-  }
+        worksheet.columns = [
+            {header: "Intern Name", key: "name", width: 30},
+            {header: "University Name", key: "universityName", width: 30},
+            {header: "Graduation Year", key: "graduationYear", width: 15},
+            {header: "Graduation Month", key: "graduationMonth", width: 15},
+            {header: "Skills", key: "skills", width: 30},
+        ];
 
-  public static async downloadBatchData(
-    res: Response,
-    batchId: mongoose.Types.ObjectId | string
-  ) {
-    const qualifications = await InternQualification.find({
-      Batch: batchId,
-    }).populate("Intern", "-_id username");
+        qualifications.forEach((q) => {
+            const {username}: any = q.Intern;
+            worksheet.addRow({
+                name: username,
+                universityName: q.universityName,
+                graduationYear: q.graduationYear,
+                graduationMonth: q.graduationMonth,
+                skills: q.skills,
+            });
+        });
 
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet("Batch Qualifications");
-
-    res.setHeader(
-      "Content-Type",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    );
-    res.setHeader(
-      "Content-Disposition",
-      `attachment; filename="batchData.xlsx"`
-    );
-
-    worksheet.columns = [
-      { header: "Intern Name", key: "name", width: 30 },
-      { header: "University Name", key: "universityName", width: 30 },
-      { header: "Graduation Year", key: "graduationYear", width: 15 },
-      { header: "Graduation Month", key: "graduationMonth", width: 15 },
-      { header: "Skills", key: "skills", width: 30 },
-    ];
-
-    qualifications.forEach((q) => {
-      const { username }: any = q.Intern;
-      worksheet.addRow({
-        name: username,
-        universityName: q.universityName,
-        graduationYear: q.graduationYear,
-        graduationMonth: q.graduationMonth,
-        skills: q.skills,
-      });
-    });
-
-    return workbook;
-  }
-
-  public static async getInternQualificationByBatch(
-    res: Response,
-    batchId: mongoose.Types.ObjectId | string,
-    next: NextFunction
-  ) {
-    const interQualifications = await InternQualification.find({
-      Batch: batchId,
-    }).populate({
-      path: "Intern",
-      select: "-_id username",
-    });
-
-    if (!interQualifications) {
-      return next(new AppError("Batch does not exist", 404));
+        return workbook;
     }
 
-    sendResponse(
-      res,
-      201,
-      "Intern Qualifications according to Batch fetched successfully !",
-      interQualifications
-    );
-  }
+    public static async getInternQualificationByBatch(
+        res: Response,
+        batchId: mongoose.Types.ObjectId | string,
+        next: NextFunction
+    ) {
+        const interQualifications = await InternQualification.find({
+            Batch: batchId,
+        }).populate({
+            path: "Intern",
+            select: "-_id username",
+        });
+
+        if (!interQualifications) {
+            return next(new AppError("Batch does not exist", 404));
+        }
+
+        sendResponse(
+            res,
+            201,
+            "Intern Qualifications according to Batch fetched successfully !",
+            interQualifications
+        );
+    }
 }
