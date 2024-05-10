335e9e44c280082481e0836b2ba09d621ddede20
iamabhas <abhas123mnm@gmail.com>
2024-05-10 13:13:39 +0545

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:3000",
-  },
+    base: "/",
+    plugins: [react()],
 });
