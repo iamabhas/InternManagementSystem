0e036d38d8876bbfa0f59c24febfb158afac7102
iamabhas <abhas123mnm@gmail.com>
2024-05-13 15:15:39 +0545

    refactor: added route protection for /dashboard

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Dashboard from "../components/dashboardComponents/Dashboard";
 
 const DashboardPage = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoggedIn]);
   return (
     <div>
       <Dashboard></Dashboard>
